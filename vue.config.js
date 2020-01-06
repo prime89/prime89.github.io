0fd90bc6c17c44e837559e7a685956650603af61
潘武 <panwu@bytedance.com>
2020-01-06 21:36:24 +0800

    fix: prefix path

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  // eslint-disable-next-line no-undef
+  publicPath:  '/dist',
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true,
+      }
+    }
+  },
+};
